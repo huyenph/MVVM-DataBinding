56b227f7df634ff0b647c001f26801fc39e76646
Huyen Pham <huyen.phamhoang96@gmail.com>
2018-10-29 10:20:26 +0700

    init project

---
+++ app/src/main/java/com/utildev/arch/basemvvm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.utildev.arch.basemvvm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
